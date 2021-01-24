9e553c23187dbc818639181c2830f7f3cfd2ba9f
nadiasb91 <nadiasb91@gmail.com>
2021-01-24 13:28:14 -0300

    Createing a Helper class

---
+++ src/test/java/Utils/Helper.java
@@ -0,0 +1,9 @@
+package Utils;
+
+public class Helper {
+    public static final String password="nbatista123";
+    public static final String email ="lola.perez910607@gmail.com";
+    public  static final String  url="hhtps://google.com";
+
+
+}
