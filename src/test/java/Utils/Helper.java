88bceb8b36ee897c9e09d97e2b576cd089f4aadc
nadiasb91 <nadiasb91@gmail.com>
2021-01-24 13:33:48 -0300

    helper url field was written wrong

--- src/test/java/Utils/Helper.java
+++ src/test/java/Utils/Helper.java
@@ -3,7 +3,7 @@ package Utils;
 public class Helper {
     public static final String password="nbatista123";
     public static final String email ="lola.perez910607@gmail.com";
-    public  static final String  url="hhtps://google.com";
+    public  static final String  url="https://google.com";
 
 
 }
